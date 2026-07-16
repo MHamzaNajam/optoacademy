65b5949d1358135b3942f92871eab62272a6a1c6
Muhammad Hamza Najam <79852455+MHamzaNajam@users.noreply.github.com>
2026-07-16 10:46:36 +0500

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -28,7 +28,6 @@ export async function middleware(request: NextRequest) {
     }
   );
 
-  // This refreshes the session if expired, and keeps cookies in sync
   await supabase.auth.getUser();
 
   return response;
@@ -36,6 +35,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|admin|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
